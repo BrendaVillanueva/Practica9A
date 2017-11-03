b483060fde2391ed7d8075b0526e4df1891dec18
BRENDA <BRENDA@DESKTOP-7O4U7A5>
2017-11-02 23:13:09 -0600

    Agregar archivos de proyecto.

---
+++ Practica9A/Practica9A/DataPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.WindowsAzure.MobileServices;
+using System.Collections.ObjectModel;
+
+namespace Practica9A
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DataPage : ContentPage
+    {
+        public ObservableCollection<_13090416> Items { get; set; }
+        public static MobileServiceClient cliente;
+        public static IMobileServiceTable<_13090416> Tabla;
+
+        public DataPage()
+        {
+            InitializeComponent();
+
+            cliente = new MobileServiceClient(AzureConnection.AzureURL);
+            Tabla = cliente.GetTable<_13090416>();
+            LeerTabla();
+
+
+        }
+
+        private void Insertar_Clicked(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new InsertPage());
+
+        }
+
+        private void Button_Mostrar_Datos_Eliminados_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DeletePage());
+        }
+
+
+        public async void LeerTabla()
+        {
+            IEnumerable<_13090416> elementos = await Tabla.ToEnumerableAsync();
+            Items = new ObservableCollection<_13090416>(elementos);
+            BindingContext = this;
+        }
+
+        async void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+            await Navigation.PushAsync(new SelectPage(e.SelectedItem as _13090416));
+        }
+
+    }
+}
---
+++ Practica9A/Practica9A/InsertPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.WindowsAzure.MobileServices;
+
+
+namespace Practica9A
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InsertPage : ContentPage
+    {
+        public InsertPage()
+        {
+            InitializeComponent();
+
+            string[] semestres = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+            Picker_Semestre.ItemsSource = semestres;
+            //PICKER CARRERAS
+            string[] carreras = { "Ing. Sistemas Computacionales", "Ing.Industrial", "Ing.Civil", "Ing. Mecatronica", "Lic.Biologia", "Lic. Administracion", "Lic.Gastronomia" };
+            Picker_Carrera.ItemsSource = carreras;
+        }
+
+        private async void Button_Insertar_Clicked(object sender, EventArgs e)
+        {
+
+            var datos = new _13090416
+            {
+                Nombre= Entry_Nombre.Text,
+                Apellidos = Entry_Apellidos.Text,
+                Direccion = Entry_Direccion.Text,
+                Telefono = Entry_Telefono.Text,
+                Carrera = Convert.ToString(Picker_Carrera.SelectedItem),
+                Semestre = Convert.ToString(Picker_Semestre.SelectedItem),
+                Correo = Entry_Correo.Text,
+                Github = Entry_Github.Text
+            };
+
+            await DataPage.Tabla.InsertAsync(datos);
+
+            await Navigation.PushAsync(new DataPage());
+
+        }
+    }
+}
